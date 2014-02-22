94c9bc5817ac73dee68cfccce1b93dca31c5a8a9
Hannah Zabarsky <h.zabarsky@gmail.com>
2014-02-21 20:20:04 -0500

    Can count one cup of coffee in basic procedural program

--- coffee_counter.rb
+++ coffee_counter.rb
@@ -1,2 +1,23 @@
 #this baby program will count how many cups of coffee you have in a day
 #let's do this.
+require 'pry'
+require 'csv'
+
+#first we're writing to a CSV file so that we can efficiently track cups of coffee
+
+#well for now, it's a blank array until i can figure out csv tracking stuff
+current_cup = 0
+current_tally_of_cups = []
+#counter
+new_cup = gets.chomp
+
+if new_cup == "coffee"
+  current_tally_of_cups << current_cup+=1
+  puts current_tally_of_cups
+else
+  puts "you need another cup huh"
+
+end
+
+
+
